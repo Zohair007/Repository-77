67e089be1b8448ae18474157b0bfa3ec28882e32
Zohair007 <89848636+Zohair007@users.noreply.github.com>
2022-01-16 13:51:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Ranbir Kapoor")
+var first_name="Ranbir"
+console.log(first_name)
+var last_name="Kapoor"
+console.log(last_name)
+var full_name=first_name.concat(last_name)
+console.log(full_name)
+var first_number=3
+console.log(first_number)
+var last_number=7
+console.log(last_number)
+var full_number=first_number+(last_number)
+console.log(full_number)
